99b45cf391c44138d43f575acb755c457480dd62
Pawan Gujral <gujral87@gmail.com>
2021-07-08 00:21:36 +0530

    add chrome type package

--- src/components/my-component.tsx
+++ src/components/my-component.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 
 const SayHello: React.FC = () => {
+
+  React.useEffect(() => {
+    // Testing purpose
+    chrome.tabs.create({
+      url: 'onboarding.html'
+    });
+  },[]);
   return <h1>Hello world!</h1>;
 };
 
